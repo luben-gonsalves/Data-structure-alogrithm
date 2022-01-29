612c8cc8242402eb4cc8e0895daf442b2ffacd95
Luben Gonsalves <luben@Lubens-MacBook-Pro.local>
2022-01-29 12:29:07 +0530

    insertion sort

---
+++ work@tech-challenges/Arrays/insertionSort.js
@@ -0,0 +1,13 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+};
+
+console.log(insertionSort([3, 11, 4, 200]));
+console.log(insertionSort([5, 4, 3, 2, 1]));
